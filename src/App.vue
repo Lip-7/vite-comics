9ea8d3a48393405edae8d83de6d5d07ab94229e3
Lip-7 <zhighofficial@gmail.com>
2023-04-13 18:21:08 +0200

    almost done footer

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <HeaderComponent/>
   <MainComponent/>
   <LinksComponent/>
+  <FooterComponent></FooterComponent>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue'
 import LinksComponent from './components/LinksComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
 export default {
   components: {
     HeaderComponent,
     MainComponent,
     LinksComponent,
+    FooterComponent,
   }
 }
 </script>
--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,13 +1,43 @@
 <template>
-    <div>
-
-    </div>
+    <footer>
+        <div class="container d-flex justify-content-between py-4 align-items-center">
+            <div class="callToAction p-3">
+                <h4 class="text-uppercase mb-0 text-white">Sign-up Now!</h4>
+            </div>
+            <div class="socials d-flex align-items-center justify-content-between gap-4">
+                <h4 class="text-uppercase mb-0">Follow us</h4>
+                <div class="socialsIcons d-flex gap-3">
+                    <img src="../assets/img/footer-facebook.png" alt="">
+                    <img src="../assets/img/footer-twitter.png" alt="">
+                    <img src="../assets/img/footer-youtube.png" alt="">
+                    <img src="../assets/img/footer-pinterest.png" alt="">
+                    <img src="../assets/img/footer-periscope.png" alt="">
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script>
 export default {
-
+    name: 'FooterComponent'
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+footer{
+    background-color: #303030;
+    .callToAction{
+        border: 2px solid #0282F9;
+        h4{
+            font-size: 1rem;
+        }
+    }
+    .socials{
+        h4{
+            font-size: 1.4rem;
+            color: #0282F9;
+        }
+    }
+}
+</style>
