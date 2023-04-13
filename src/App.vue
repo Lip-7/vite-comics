24f9c6d9b22a8e3bad8619a9a3edb98626ae2b01
Lip-7 <zhighofficial@gmail.com>
2023-04-13 17:54:02 +0200

    almost done links component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <HeaderComponent/>
   <MainComponent/>
+  <LinksComponent/>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue'
+import LinksComponent from './components/LinksComponent.vue';
 export default {
   components: {
     HeaderComponent,
     MainComponent,
+    LinksComponent,
   }
 }
 </script>
