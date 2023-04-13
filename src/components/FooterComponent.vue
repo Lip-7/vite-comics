d65fa0d89b57dab5227cefa5a5a0c78042edd06f
Lip-7 <zhighofficial@gmail.com>
2023-04-13 18:24:04 +0200

    minor fixes on footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,16 +2,16 @@
     <footer>
         <div class="container d-flex justify-content-between py-4 align-items-center">
             <div class="callToAction p-3">
-                <h4 class="text-uppercase mb-0 text-white">Sign-up Now!</h4>
+                <a class="text-decoration-none" href="#"><h4 class="text-uppercase mb-0 text-white">Sign-up Now!</h4></a>
             </div>
             <div class="socials d-flex align-items-center justify-content-between gap-4">
                 <h4 class="text-uppercase mb-0">Follow us</h4>
                 <div class="socialsIcons d-flex gap-3">
-                    <img src="../assets/img/footer-facebook.png" alt="">
-                    <img src="../assets/img/footer-twitter.png" alt="">
-                    <img src="../assets/img/footer-youtube.png" alt="">
-                    <img src="../assets/img/footer-pinterest.png" alt="">
-                    <img src="../assets/img/footer-periscope.png" alt="">
+                    <a href="#"><img src="../assets/img/footer-facebook.png" alt=""></a>
+                    <a href="#"><img src="../assets/img/footer-twitter.png" alt=""></a>
+                    <a href="#"><img src="../assets/img/footer-youtube.png" alt=""></a>
+                    <a href="#"><img src="../assets/img/footer-pinterest.png" alt=""></a>
+                    <a href="#"><img src="../assets/img/footer-periscope.png" alt=""></a>
                 </div>
             </div>
         </div>
